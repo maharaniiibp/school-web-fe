d8fa6eb59f0d4d5e6cb6de556cd611f9ce198cda
Maharaniiibp <maharanibanicaputri@gmail.com>
2023-09-04 23:02:02 +0700

    firstcommit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: {
+     content: ["./public/**/*.{html,js}"],
+  
+  },
+ theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
